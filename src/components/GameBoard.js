21048d937fdfcbff2f65e45469fce22bcbe8ca45
Rajiv Poddar <rajiv@gimlet.ai>
2023-11-30 12:02:49 +0530

    Implement game win and tie detection logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GameBoard = () => {
-  const grid = Array(9).fill(null);
+  const [grid, setGrid] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const checkForWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleClick = index => {
+    if (grid[index] || checkForWinner(grid)) return;
+    const newGrid = [...grid];
+    newGrid[index] = isXNext ? 'X' : 'O';
+    setGrid(newGrid);
+    setIsXNext(!isXNext);
+  };
+
+  const winner = checkForWinner(grid);
+  let status;
+  if (winner) {
+    status = 'Player ' + winner + ' Wins!';
+  } else if (grid.every(cell => cell != null)) {
+    status = 'Game is a tie!';
+  } else {
+    status = 'Next player: ' + (isXNext ? 'X' : 'O');
+  }
 
   return (
     <div className='game-grid'>
       {grid.map((value, index) => (
-        <button key={index} className='grid-cell' role='button'>
+        <button key={index} className='grid-cell' role='button' onClick={() => handleClick(index)}>
           {value}
         </button>
       ))}
+      <div>{status}</div>
     </div>
   );
 };
--- src/tests/GameBoard.test.js
+++ src/tests/GameBoard.test.js
@@ -10,3 +10,42 @@ test('renders a 3x3 game grid with empty cells', () => {
     expect(cell).toHaveTextContent('');
   });
 });
+
+test('allows players to take turns clicking on cells to place X and O', () => {
+  const { getAllByRole } = render(<GameBoard />);
+  const cells = getAllByRole('button');
+  fireEvent.click(cells[0]);
+  expect(cells[0]).toHaveTextContent('X');
+  fireEvent.click(cells[1]);
+  expect(cells[1]).toHaveTextContent('O');
+  fireEvent.click(cells[2]);
+  expect(cells[2]).toHaveTextContent('X');
+});
+
+test('detects a win for a player', () => {
+  const { getAllByRole, getByText } = render(<GameBoard />);
+  const cells = getAllByRole('button');
+  // Simulate a win for Player X
+  fireEvent.click(cells[0]); // X
+  fireEvent.click(cells[3]); // O
+  fireEvent.click(cells[1]); // X
+  fireEvent.click(cells[4]); // O
+  fireEvent.click(cells[2]); // X
+  expect(getByText('Player X Wins!')).toBeInTheDocument();
+});
+
+test('detects a tie game', () => {
+  const { getAllByRole, getByText } = render(<GameBoard />);
+  const cells = getAllByRole('button');
+  // Simulate a tie game
+  fireEvent.click(cells[0]); // X
+  fireEvent.click(cells[1]); // O
+  fireEvent.click(cells[2]); // X
+  fireEvent.click(cells[4]); // O
+  fireEvent.click(cells[3]); // X
+  fireEvent.click(cells[5]); // O
+  fireEvent.click(cells[7]); // X
+  fireEvent.click(cells[6]); // O
+  fireEvent.click(cells[8]); // X
+  expect(getByText('Game is a tie!')).toBeInTheDocument();
+});
